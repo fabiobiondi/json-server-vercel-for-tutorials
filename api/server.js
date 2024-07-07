8c98ea84524cf2a0ed0dc53b5a5094ff7c4930df
Fabio Biondi <fabiobiondi@users.noreply.github.com>
2024-07-07 16:00:10 +0200

    Update server.js

--- api/server.js
+++ api/server.js
@@ -1,29 +1,22 @@
-const fs = require('fs');
 const path = require('path');
 const jsonServer = require('json-server');
+const server = jsonServer.create();
+const middlewares = jsonServer.defaults();
 
 // Require the JSON data directly
 const db = require('./db.json');
 
-// See https://github.com/typicode/json-server#module
-const server = jsonServer.create();
-
-// Uncomment to allow write operations
-const filePath = path.join('db.json');
-const data = fs.readFileSync(filePath, 'utf-8');
-
-// Comment out to allow write operations
+// Create a router using the loaded JSON data
 const router = jsonServer.router(db);
 
-const middlewares = jsonServer.defaults();
-
 server.use(middlewares);
+
 // Add this before server.use(router)
 server.use(jsonServer.rewriter({
-    // '/todos/*': '/$1',
     '/api/*': '/$1',
     '/blog/:resource/:id/show': '/:resource/:id'
 }));
+
 server.use(router);
 server.listen(3000, () => {
     console.log('JSON Server is running');
